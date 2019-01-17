3a995976bdf93633e546d16ba9523df142c70d2b
josepheastman <josephryan96@gmail.com>
2019-01-17 15:57:31 -0800

    Formatted

--- helpers/accessFile.js
+++ helpers/accessFile.js
@@ -1,37 +1,35 @@
 const knex = require("knex");
 const knexConfig = require("./knexfile.js");
-const db = require('../knexfile.js')
-
+const db = require("../knexfile.js");
 
 function getDishes() {
-    return db('dishes')
+  return db("dishes");
 }
 
 function addDish(dish) {
-    return db('dishes')
+  return db("dishes")
     .insert(dish)
-    .then(id => id[0])
+    .then(id => id[0]);
 }
 
 function getDish(id) {
-    return db(dishes)
-    .where('id', id)
+  return db(dishes).where("id", id);
 }
 
 function getRecipes() {
-    return db('recipes')
+  return db("recipes");
 }
 
 function addRecipe(recipe) {
-    return db('recipes')
+  return db("recipes")
     .insert(recipe)
-    .then(id => id[0])
+    .then(id => id[0]);
 }
 
 module.exports = {
-    getDishes,
-    addDish,
-    getDish,
-    getRecipes,
-    addRecipe
-}
+  getDishes,
+  addDish,
+  getDish,
+  getRecipes,
+  addRecipe
+};
--- index.js
+++ index.js
@@ -13,7 +13,6 @@ const db = knex(knexConfig.development);
 
 const port = 5000;
 
-
 server.listen(port, function() {
-    console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
-  });
+  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+});
